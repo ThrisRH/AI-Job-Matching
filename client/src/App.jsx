8a9deb74435a0fbd167d0c04022724b4b78c4410
ThrisRH <144113163+ThrisRH@users.noreply.github.com>
2025-08-04 21:02:45 +0700

    scss installed

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.scss";
+
+const App = () => {
+  return (
+    <div className="Box">
+      <h1>Hello World</h1>
+      <h2>Hello Bro</h2>
+    </div>
+  );
+};
+
+export default App;
